6b1cdd83cee8e73949bef59158b7cf8a72809f74
aketkar18 <anujketkar1118@gmail.com>
2023-03-11 22:25:10 -0600

    Entirely ChatGPT written currently

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [statements, setStatements] = useState(["", "", ""]);
+  const [quiz, setQuiz] = useState(null);
+
+  const handleChange = (event, index) => {
+    const newStatements = [...statements];
+    newStatements[index] = event.target.value;
+    setStatements(newStatements);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch("https://practest.herokuapp.com/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ statements }),
+    });
+
+    const quizData = await response.json();
+    console.log(quizData);
+    setQuiz(quizData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="statement1">Statement 1</label>
+          <input
+            type="text"
+            className="form-control"
+            id="statement1"
+            value={statements[0]}
+            onChange={(event) => handleChange(event, 0)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="statement2">Statement 2</label>
+          <input
+            type="text"
+            className="form-control"
+            id="statement2"
+            value={statements[1]}
+            onChange={(event) => handleChange(event, 1)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="statement3">Statement 3</label>
+          <input
+            type="text"
+            className="form-control"
+            id="statement3"
+            value={statements[2]}
+            onChange={(event) => handleChange(event, 2)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary mt-3">
+          Generate Quiz
+        </button>
+      </form>
+      {quiz && (
+        <div className="mt-5">
+          {Object.keys(quiz).map((key) => {
+            const question = quiz[key];
+            return (
+              <div key={key}>
+                <h5>{question.question}</h5>
+                <ul>
+                  {question.choices.map((choice, index) => (
+                    <li key={index}>{choice}</li>
+                  ))}
+                </ul>
+                <p>Answer: {question.answer}</p>
+                <p>{question.explanation}</p>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
