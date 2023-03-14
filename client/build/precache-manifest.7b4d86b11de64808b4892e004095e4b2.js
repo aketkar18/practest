aa3330373c211bce0161efd7c711b4aa201d03fb
aketkar18 <anujketkar1118@gmail.com>
2023-03-14 18:22:38 -0500

    Added build folder

---
+++ client/build/precache-manifest.7b4d86b11de64808b4892e004095e4b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "42d789a2067d71770565",
+    "url": "/static/css/main.ca6002b2.chunk.css"
+  },
+  {
+    "revision": "42d789a2067d71770565",
+    "url": "/static/js/main.61da7183.chunk.js"
+  },
+  {
+    "revision": "037431c4b892b05fcb8c",
+    "url": "/static/js/runtime~main.2010c6ad.js"
+  },
+  {
+    "revision": "387cf18d5638e7109dda",
+    "url": "/static/css/2.86579107.chunk.css"
+  },
+  {
+    "revision": "387cf18d5638e7109dda",
+    "url": "/static/js/2.efbeb865.chunk.js"
+  },
+  {
+    "revision": "c563299317d52c1696cd",
+    "url": "/static/js/3.937f6cc8.chunk.js"
+  },
+  {
+    "revision": "400a07c52c69626a8304165fceea9037",
+    "url": "/index.html"
+  }
+];
