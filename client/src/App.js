7d806f663d6f2c774b0c319110eeb9f79a510949
aketkar18 <anujketkar1118@gmail.com>
2023-03-16 18:32:27 -0500

    Multiple reactive components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "./Form";
+import LoadingScreen from "./LoadingScreen";
+import Quiz from "./Quiz";
 
 function App() {
   const [topics, setTopics] = useState(["", "", ""]);
   const [quiz, setQuiz] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [clicked, setClicked] = useState([false, false, false]);
 
   const handleChange = (event, index) => {
@@ -11,9 +15,11 @@ function App() {
     newTopics[index] = event.target.value;
     setTopics(newTopics);
   };
-  
+
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setIsLoading(true);
+
     let apiUrl = "https://practest-server.herokuapp.com/quiz";
 
     if (process.env.NODE_ENV === "development") {
@@ -29,100 +35,53 @@ function App() {
     });
 
     const quizData = await response.json();
-    console.log(quizData);
     setQuiz(quizData);
+    setIsLoading(false);
   };
-  
+
+  const handleAnswer = (questionIndex, isCorrect, explanation) => {
+    setClicked((prevClicked) => {
+      const newClicked = { ...prevClicked };
+      newClicked[questionIndex - 1] = true;
+      return newClicked;
+    });
+
+    if (!isCorrect) {
+      alert(explanation);
+    }
+  };
+
   return (
     <div>
-        <div className="header bg-dark py-5">
-          <h1 className="text-white text-center display-1" style={{ fontFamily: "'Roboto', sans-serif", fontWeight: "bold" }}>Practest</h1>
-          <h6 className="text-white text-center" style={{ fontFamily: "'Roboto', sans-serif", fontWeight: "bold" }}>GPT-3 Powered Practice Tests</h6>
-        </div>
+      <div className="header bg-dark py-5">
+        <h1
+          className="text-white text-center display-1"
+          style={{
+            fontFamily: "'Roboto', sans-serif",
+            fontWeight: "bold",
+          }}
+        >
+          Practest
+        </h1>
+        <h6
+          className="text-white text-center"
+          style={{
+            fontFamily: "'Roboto', sans-serif",
+            fontWeight: "bold",
+          }}
+        >
+          ChatGPT Powered Practice Tests
+        </h6>
+      </div>
       <div className="container-fluid px-5">
-      <div style={{margin: "20px"}}>
-        <p className="text-center">Enter topics from your notes in the boxes below to generate a quiz using the topics.</p>
+        {isLoading && <LoadingScreen />}
+        {!isLoading && !quiz && (
+          <Form topics={topics} onSubmit={handleSubmit} onChange={handleChange} />
+        )}
+        {!isLoading && quiz && (
+          <Quiz quiz={quiz} onAnswer={handleAnswer} clicked={clicked} />
+        )}
       </div>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="topic1">Topic 1</label>
-          <input
-            type="text"
-            className="form-control"
-            id="topic1"
-            value={topics[0]}
-            onChange={(event) => handleChange(event, 0)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="topic2">Topic 2</label>
-          <input
-            type="text"
-            className="form-control"
-            id="topic2"
-            value={topics[1]}
-            onChange={(event) => handleChange(event, 1)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="topic3">Topic 3</label>
-          <input
-            type="text"
-            className="form-control"
-            id="topic3"
-            value={topics[2]}
-            onChange={(event) => handleChange(event, 2)}
-          />
-        </div>
-        <div className="text-center mt-3">
-        <button type="submit" className="btn btn-primary mt-4" style={{ 
-          backgroundColor: '#008CBA',
-          borderColor: '#008CBA',
-          color: '#fff',
-          borderRadius: '5px',
-          padding: '10px 20px',
-          fontWeight: 'bold',
-          fontSize: '1rem'
-        }}>
-          Generate Quiz
-        </button>
-        </div>
-      </form>
-      </div>
-      {quiz && (
-        <div className="container mt-5 mx-auto px-3"> 
-          {Object.keys(quiz).map((key) => {
-            const question = quiz[key];
-            return (
-              <div key={key}>
-                <h5>{question.question}</h5>
-                <ul>
-                  {question.choices.map((choice, index) => {
-                    var choices = ['A', 'B', 'C', 'D','E']
-                    const isCorrect = choices[index] === question.answer;
-                    const className = `answer-choice ${clicked[key-1] ? (isCorrect ? 'correct' : 'incorrect') : ''}`;
-                    const onClick = () => {
-                      setClicked(prevClicked => {
-                        const newClicked = { ...prevClicked };
-                        newClicked[key-1] = true;
-                        return newClicked;
-                      });
-                      if (!isCorrect) {
-                        alert(question.explanation);
-                      }
-                    };                    
-                    return (
-                      <li key={index} className={className} onClick={onClick}>
-                        {choice}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
-            );
-          })}
-        </div>
-)}
     </div>
   );
 }
