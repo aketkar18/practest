46ff17ce3f33e446fc69c4d478fbb44d58674f4b
aketkar18 <anujketkar1118@gmail.com>
2023-03-16 19:33:58 -0500

    Prompt Change

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ async function hello_world() {
 }
 
 async function make_quiz(topics) {
-  let prompt = "I need to make a multiple choice exam. I am going to give you a list of facts and I want you to make a multiple choice questions for each fact. The questions should have 5 answer choices, one of which is correct, but be sure to not make the same letter the answer in a row too much. Give the all the questions and their answer choices first in a list, then start a new list for the correct answers, and finally the explanations in a list. Clearly label these lists as Questions, Answers, or Explanations. There should be exactly" + topics.length + "questions. Here are the facts: ";
+  let prompt = "I need to make a multiple choice exam. I am going to give you a list of topics and I want you to make one college level multiple choice question for each topic. The questions should have 5 answer choices, only one of which is correct. Give the all the questions and their answer choices first in a list, then start a new list for the correct answers, and finally the explanations. Clearly label these lists as Questions, Answers, or Explanations. There should be exactly" + topics.length + " questions. Prioritize having enough space to include the Answer section written over long questions. Here are the topics: ";
   for (let i = 0; i < topics.length; i++) {
     prompt += topics[i] + "\n";
   }
